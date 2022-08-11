ace5a81ea396a97bba2f29dcbbd43cb00490f154
Chris Snow <christopher.snow@austincc.edu>
2022-08-11 14:01:20 -0500

    Day 4

--- src/main/java/com/galvanize/NameEmailFormatter.java
+++ src/main/java/com/galvanize/NameEmailFormatter.java
@@ -1,6 +1,15 @@
 package com.galvanize;
 
 public class NameEmailFormatter {
-
-
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please specify a name and email");
+        }
+        else if (args.length == 1) {
+            System.out.println("Please specify an email for " + args[0]);;
+        }
+        else {
+            System.out.println(args[0] + " <" + args[1] + ">");
+        }
+    }
 }
